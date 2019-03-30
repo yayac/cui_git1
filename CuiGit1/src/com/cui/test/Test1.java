5bfcf18703c02bc907261d0ec9d25fd5caf8d148
147239751 <147239751@qq.com>
2019-03-30 16:01:00 +0800

    测试提交到branch2

--- CuiGit1/src/com/cui/test/Test1.java
+++ CuiGit1/src/com/cui/test/Test1.java
@@ -2,6 +2,6 @@ package com.cui.test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("ccccccccc2222222222222222");
+		System.out.println("ccccccccc2222222222222222");   
 	}
 }
